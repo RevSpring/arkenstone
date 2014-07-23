e842efa9ff823d90d7b845f0efb755bef8f2f638
Tom Dunlap <apocryphalauthor@gmail.com>
2014-07-23 15:34:00 -0400

    Implement client_only_attributes (addresses #70)
    - change behavior of `Document.attributes` to create or add to existing
      attributes
    - add `Document.client_only_attributes` which adds to `attributes` but also tags
      these attribute in such a way as to be not be serialized by
      `#saveable_attributes`
    - refactor `#saveable_attributes`
    - update tests

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -8,3 +8,22 @@ require 'pry-nav'
 #Make a simple document for test cases.
 require_relative '../lib/arkenstone.rb'
 Dir['./test/dummy/app/models/**/*.rb'].each { |f| require f }
+
+def build_user(id)
+  User.build user_options.merge({id: id})
+end
+
+def create_user(options, id)
+  stub_request(:post, User.arkenstone_url).to_return(body: options.merge({id: id}).to_json)
+  User.build(options).save
+end
+
+def user_options(options={})
+  {
+    name: 'John Doe',
+    age: 18,
+    gender: 'Male',
+    bearded: true,
+    awesome: true
+  }.merge!(options)
+end
